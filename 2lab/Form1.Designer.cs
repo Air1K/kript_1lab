a88da6714dc3e9dcdb168b494786b39af42f523b
Air1K <veremeenkom91@gmail.com>
2023-03-07 01:09:32 +0500

    AES and DES

---
+++ 2lab/Form1.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace _2lab
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button1 = new Button();
+            button2 = new Button();
+            textBox1 = new TextBox();
+            label1 = new Label();
+            saveFileDialog1 = new SaveFileDialog();
+            openFileDialog1 = new OpenFileDialog();
+            comboBox1 = new ComboBox();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(57, 119);
+            button1.Name = "button1";
+            button1.Size = new Size(125, 29);
+            button1.TabIndex = 0;
+            button1.Text = "Шифровать";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(57, 153);
+            button2.Name = "button2";
+            button2.Size = new Size(125, 29);
+            button2.TabIndex = 1;
+            button2.Text = "Расшифровать";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(57, 52);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(125, 27);
+            textBox1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(57, 29);
+            label1.Name = "label1";
+            label1.Size = new Size(33, 20);
+            label1.TabIndex = 3;
+            label1.Text = "Key";
+            label1.Click += label1_Click;
+            // 
+            // openFileDialog1
+            // 
+            openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Items.AddRange(new object[] { "DES-64", "AES" });
+            comboBox1.Location = new Point(57, 85);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(125, 28);
+            comboBox1.TabIndex = 4;
+            comboBox1.SelectedIndexChanged += comboBox1_SelectedIndexChanged;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(242, 198);
+            Controls.Add(comboBox1);
+            Controls.Add(label1);
+            Controls.Add(textBox1);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button button1;
+        private Button button2;
+        private TextBox textBox1;
+        private Label label1;
+        private SaveFileDialog saveFileDialog1;
+        private OpenFileDialog openFileDialog1;
+        private ComboBox comboBox1;
+    }
+}
---
+++ 2lab/Form1.cs
@@ -0,0 +1,188 @@
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+using System.Windows.Forms;
+
+namespace _2lab
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+            comboBox1.SelectedIndex = 0;
+        }
+        private static byte[] GetIV(string ivSecret)
+        {
+            using MD5 md5 = MD5.Create();
+            return md5.ComputeHash(Encoding.UTF8.GetBytes(ivSecret));
+        }
+        private static byte[] GetKey(string key)
+        {
+            using SHA256 sha256 = SHA256.Create();
+            return sha256.ComputeHash(Encoding.UTF8.GetBytes(key));
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+        static string sKey;
+        static string ivSecret = "вектор";
+        static string values = "";
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            sKey = textBox1.Text;
+                if (openFileDialog1.ShowDialog() == DialogResult.OK)
+                {
+                    string source = openFileDialog1.FileName;
+                    if(values == "AES") { saveFileDialog1.Filter = "enc files |*.enc"; }
+                    else { saveFileDialog1.Filter = "des files |*.des"; }
+                    
+                    if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+                    {
+                        string destination = saveFileDialog1.FileName; // Переменная в которую сохроняем зашифрованный файл
+                        if (values == "AES")
+                        {
+                            EncryptFileAes(source, destination, ivSecret, sKey);
+                        }
+                        else { EncryptFile(source, destination, sKey); }
+                        
+                    }
+                }
+        }
+        private void EncryptFileAes(string source, string destination, string ivSecret, string sKey)
+        {
+            using Aes aes = Aes.Create();
+            aes.IV = GetIV(ivSecret);
+            aes.Key = GetKey(sKey);
+            using FileStream inStream = new FileStream(source, FileMode.Open); //создаем файловый поток на чтение
+            using FileStream outStream = new FileStream(destination, FileMode.Create);//создаем файловый поток на запись
+                                                                                      //поток для шифрования данных
+            CryptoStream encStream = new CryptoStream(outStream, aes.CreateEncryptor(aes.Key, aes.IV), CryptoStreamMode.Write);
+            long readTotal = 0;
+
+            int len;
+            int tempSize = 100; //размер временного хранилища
+            byte[] bin = new byte[tempSize];    //временное Хранилище для зашифрованной информации
+            while (readTotal < inStream.Length)
+            {
+                len = inStream.Read(bin, 0, tempSize);
+                encStream.Write(bin, 0, len);
+                readTotal = readTotal + len;
+                MessageBox.Show($"{readTotal} байт обработано");
+            }
+            encStream.Close();
+            outStream.Close();
+            inStream.Close();
+        }
+        private void EncryptFile(string source, string destination, string sKey)
+        {
+            FileStream fsInput = new FileStream(source, FileMode.Open, FileAccess.Read);
+            FileStream fsEncrypted = new FileStream(destination, FileMode.Create, FileAccess.Write);
+            DESCryptoServiceProvider DES = new DESCryptoServiceProvider();
+            try
+            {
+                DES.Key = ASCIIEncoding.ASCII.GetBytes(sKey);
+                DES.IV = ASCIIEncoding.ASCII.GetBytes(sKey);
+                ICryptoTransform desencrypt = DES.CreateEncryptor();
+                CryptoStream cryptostream = new CryptoStream(fsEncrypted, desencrypt, CryptoStreamMode.Write);
+                byte[] bytearrayinput = new byte[fsInput.Length - 0];
+                fsInput.Read(bytearrayinput, 0, bytearrayinput.Length);
+                cryptostream.Write(bytearrayinput, 0, bytearrayinput.Length);
+                cryptostream.Close();
+            }
+            catch
+            {
+                MessageBox.Show("Ошибка");
+                return;
+            }
+            fsInput.Close();
+            fsEncrypted.Close();
+
+        }
+
+        private void DecryptFile(string source, string destination, string sKey)
+        {
+            FileStream fsInput = new FileStream(source, FileMode.Open, FileAccess.Read);
+            FileStream fsEncrypted = new FileStream(destination, FileMode.Create, FileAccess.Write);
+            DESCryptoServiceProvider DES = new DESCryptoServiceProvider();
+            try
+            {
+                DES.Key = ASCIIEncoding.ASCII.GetBytes(sKey);
+                DES.IV = ASCIIEncoding.ASCII.GetBytes(sKey);
+                ICryptoTransform desencrypt = DES.CreateDecryptor();
+                CryptoStream cryptostream = new CryptoStream(fsEncrypted, desencrypt, CryptoStreamMode.Write);
+                byte[] bytearrayinput = new byte[fsInput.Length - 0];
+                fsInput.Read(bytearrayinput, 0, bytearrayinput.Length);
+                cryptostream.Write(bytearrayinput, 0, bytearrayinput.Length);
+                cryptostream.Close();
+            }
+            catch
+            {
+                MessageBox.Show("Ошибка");
+            }
+            fsInput.Close();
+            fsEncrypted.Close();
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            sKey = textBox1.Text;
+            if (values == "AES") { openFileDialog1.Filter = "enc files |*.enc"; }
+            else { openFileDialog1.Filter = "des files|*.des"; }
+            
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                string source = openFileDialog1.FileName;
+                saveFileDialog1.Filter = "txt files |*.txt";
+                if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+                {
+                    string destination = saveFileDialog1.FileName; // Переменная в которую сохроняем зашифрованный файл
+                    if (values == "AES")
+                    {
+                        DecryptFileAes(source, destination, GetKey(sKey), GetKey(ivSecret));
+                    }
+                    else { DecryptFile(source, destination, sKey); }
+                    
+                }
+            }
+        }
+        private void DecryptFileAes(string source, string destination, byte[] key, byte[] iv)
+        {
+            using FileStream fileStream = new(source, FileMode.Open);
+            using Aes aes = Aes.Create();
+
+            aes.IV = GetIV(ivSecret);
+
+            using CryptoStream cryptoStream = new(fileStream,
+                                       aes.CreateDecryptor(key, aes.IV),
+                                                  CryptoStreamMode.Read); //создаем поток для чтения (расшифровки) данных
+            using FileStream outStream = new FileStream(destination, FileMode.Create);//создаем поток для расшифрованных данных
+
+            using BinaryReader decryptReader = new(cryptoStream);
+            int tempSize = 10;  //размер временного хранилища
+            byte[] data;        //временное хранилище для зашифрованной информации
+            while (true)
+            {
+                data = decryptReader.ReadBytes(tempSize);
+                if (data.Length == 0)
+                    break;
+                outStream.Write(data, 0, data.Length);
+            }
+        }
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            values = comboBox1.Text;
+        }
+    }
+}
